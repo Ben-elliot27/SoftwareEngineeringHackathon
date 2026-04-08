2ff4b6416fdaea3bc14e6ddbc65f71c1a62c324d
Chanel Morgan <chanel.morgan@cgi.com>
2026-04-08 12:00:12 +0100

    routes

--- timsheet-frontend/src/App.tsx
+++ timsheet-frontend/src/App.tsx
@@ -1,16 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import TimesheetApprovals from "./pages/DirectorPage.tsx";
 import AddTimesheet from "./pages/addTimesheet.tsx";
 import TimesheetWebsite from "./pages/Home.tsx";
 
 export default function App() {
-  return <>
-  
-   {/* <AddTimesheet /> */}
-   {/* <TimesheetWebsite /> */}
-   <TimesheetApprovals />
-
-  </>
-;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TimesheetWebsite />} />
+        <Route path="/add-timesheet" element={<AddTimesheet />} />
+        <Route path="/approvals" element={<TimesheetApprovals />} />
+      </Routes>
+    </Router>
+  );
 }
-
- 
