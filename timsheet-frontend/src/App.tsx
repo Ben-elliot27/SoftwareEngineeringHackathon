17fdc2f58f511b277eaba0afa458a4831cc4bad9
Nash <romilly.nash@cgi.com>
2026-04-08 15:13:50 +0100

    Addes history page

--- timsheet-frontend/src/App.tsx
+++ timsheet-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import TimesheetApprovals from "./pages/DirectorPage";
 import AddTimesheet from "./pages/addTimesheet";
 import TimesheetWebsite from "./pages/Home";
 import WeeklyTimesheetPage from "./pages/ViewTimesheet";
+import History from "./pages/history";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<TimesheetWebsite />} />
         <Route path="/add-timesheet" element={<AddTimesheet />} />
+        <Route path="/history" element={<History />} />
         <Route path="/approvals" element={<TimesheetApprovals />} />
         <Route path="/director-page" element={<TimesheetApprovals />} />
         <Route path="/view-timesheet" element={<WeeklyTimesheetPage />} />
